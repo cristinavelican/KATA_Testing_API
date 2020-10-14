ae21fdf430325f6374c95e2678249677adc60e33
Cristina <cristina.velican@sogeti.com>
2020-10-14 16:10:29 +0200

    add necessary helpes

--- helpers/helpers.rb
+++ helpers/helpers.rb
@@ -13,6 +13,19 @@ module Helpers
     response
   end
 
+  def call_post path, body
+    uri = URI.parse(BASE_URL+path)
+    begin
+      http = Net::HTTP.new(uri.host,uri.port)
+      req = Net::HTTP::Post.new(uri)
+      req["Content-Type"] = "application/json"
+      req.body = body.to_json
+    rescue => exception
+      raise exception
+    end
+    http.request(req)
+  end
+
   
 
 end
